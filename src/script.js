f4c87420f298187f582582414e22093f70111d7b
Douglas Braz Batista <doug.batista@live.com>
2023-01-06 12:40:36 -0300

    fix: feita as correções da funcionalidade e falta apenas a responsividade

--- src/script.js
+++ src/script.js
@@ -2,12 +2,17 @@ const red = document.getElementById ("red");
 const yellow = document.getElementById("yellow");
 const green = document.getElementById("green");
 
+/*traffic light colors 2*/
+
+const red2 = document.getElementById ("red2");
+const yellow2 = document.getElementById("yellow2");
+const green2 = document.getElementById("green2");
+
 //when the page is loaded, it calls the automation function.
-// window.onload = automation;
 
-//function to automate the change of traffic light colors.
+window.onload = automation;
 
-let time = setTimeout(automation, 0);   
+//function to automate the change of traffic light colors 
 
 function automation () { 
   
@@ -25,29 +30,50 @@ function automation () {
         yellow.classList.remove ("yellowLightOn")        
         red.classList.add ("redLightOn");
         
-        // setTimeout(() => {          
-        //   time = automation();          
-        // }, 2000);
+        setTimeout(() => {          
+          time = automation();          
+        }, 2000);
       }, 2000);
-    }, 2000);     
-    
+    }, 2000);  
   }, 0);  
- 
 }
 
 function turnOn () {  
-   automation();
- 
-  console.log("turnOn")  
+  document.getElementById("semaphore").style.display = "flex";
+  document.getElementById("semaphore2").style.display = "none";
+  window.onload
+
+  red2.classList.remove ("redLightOn")
+  yellow2.classList.remove ("yellowLightOn");
+  green2.classList.remove ("greenLightOn");
 }
 
-function turnOff (){  
+function turnOff (){ 
+  document.getElementById("semaphore").style.display = "none";
+  document.getElementById("semaphore2").style.display = "flex";
 
-  clearTimeout(time)
-  console.log("desligou")
-  
+  red2.classList.remove ("redLightOn")
+  yellow2.classList.remove ("yellowLightOn");
+  green2.classList.remove ("greenLightOn");
 } 
 
+function redLight (){
+  red2.classList.add ("redLightOn");
+  yellow2.classList.remove ("yellowLightOn");
+  green2.classList.remove ("greenLightOn");
+}
+
+function yellowLight (){
+  yellow2.classList.add ("yellowLightOn");
+  red2.classList.remove ("redLightOn")
+  green2.classList.remove ("greenLightOn");
+}
+
+function greenLight (){
+  green2.classList.add ("greenLightOn");
+  red2.classList.remove ("redLightOn")
+  yellow2.classList.remove ("yellowLightOn");
+}
 
 
 
